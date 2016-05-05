13ca452586eba099e9c3d1d108af97c5c62e6e20
Donatas Stundys <donatas.stundys@necolt.com>
2016-05-05 10:38:17 +0300

    Support 05-03 snapshot

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "PathParameterMiddleware",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6)
     ]
 )
