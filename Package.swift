7fd8b144a49185aa30ed8cab7ca72864a73e63bc
Dan Appel <dan.appel00@gmail.com>
2016-06-08 00:04:54 -0700

    Update to 05-09 & bump dependencies

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "PathParameterMiddleware",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 8)
     ]
 )
